07ec3cc8757e1ef2a859c116af53c7bbb07cf0f5
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2021-11-25 10:10:38 +0100

    Check for caps corectness
    Optimilize timestamp calculation

--- lib/blank_video_generator.ex
+++ lib/blank_video_generator.ex
@@ -29,14 +29,15 @@ defmodule Membrane.BlankVideoGenerator do
 
   @impl true
   def handle_init(opts) do
-    inspect(opts)
+    %Raw{framerate: {frames, seconds}} = opts.caps
 
-    if Matcher.match?(@supported_caps, opts.caps) do
+    if caps_correct?(opts.caps) do
       state =
         opts
         |> Map.from_struct()
-        |> Map.put(:passed_frames, 0)
+        |> Map.put(:current_ts, Ratio.new(0, frames))
         |> Map.put(:frame, blank_frame(opts.caps))
+        |> Map.put(:ts_increment, Ratio.new(seconds |> Time.seconds(), frames))
 
       {:ok, state}
     else
@@ -58,6 +59,16 @@ defmodule Membrane.BlankVideoGenerator do
     {{:ok, caps: {:output, caps}}, state}
   end
 
+  defp caps_correct?(caps), do: Matcher.match?(@supported_caps, caps) && do_caps_correct?(caps)
+
+  defp do_caps_correct?(%Raw{format: :I420, width: width, height: height}) do
+    rem(height, 2) == 0 && rem(width, 2) == 0
+  end
+
+  defp do_caps_correct?(%Raw{format: :I422, width: width}) do
+    rem(width, 2) == 0
+  end
+
   defp get_buffers(size, state, acc \\ [])
   defp get_buffers(0, state, acc), do: {Enum.reverse(acc), state}
 
@@ -65,7 +76,7 @@ defmodule Membrane.BlankVideoGenerator do
     {ts, new_state} = get_timestamp(state)
 
     if ts < duration do
-      buffer = %Buffer{payload: frame, metadata: %{pts: ts}}
+      buffer = %Buffer{payload: frame, pts: ts}
       get_buffers(size - 1, new_state, [buffer | acc])
     else
       {:eos, Enum.reverse(acc), state}
@@ -82,11 +93,12 @@ defmodule Membrane.BlankVideoGenerator do
       :binary.copy(<<128>>, height * width)
   end
 
-  defp get_timestamp(%{caps: %Raw{framerate: {frames, seconds}}, passed_frames: passed} = state) do
+  defp get_timestamp(%{current_ts: current_ts, ts_increment: ts_increment} = state) do
     use Ratio
 
-    timestamp = (passed * seconds * Time.second()) |> Ratio.new(frames) |> Ratio.trunc()
-    state = %{state | passed_frames: passed + 1}
-    {timestamp, state}
+    new_ts = current_ts + ts_increment
+    result_ts = current_ts |> Ratio.trunc()
+    state = %{state | current_ts: new_ts}
+    {result_ts, state}
   end
 end
