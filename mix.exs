f17e47bd44c47b041b4751c2ce32bbe749ad8392
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2021-12-09 17:23:15 +0100

    Bump version to 0.2.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Template.Mixfile do
   use Mix.Project
 
-  @version "0.2.2"
+  @version "0.2.3"
   @github_url "https://github.com/membraneframework/membrane_generator_plugin"
 
   def project do
