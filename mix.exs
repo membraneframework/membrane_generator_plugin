485a2a2699587f471ec67b7160b5274350378c3b
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2023-01-31 17:24:11 +0100

    Bump version `0.7.1` ~> `0.8.0`

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Generator.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.7.1"
+  @version "0.8.0"
   @github_url "https://github.com/membraneframework/membrane_generator_plugin"
 
   def project do
