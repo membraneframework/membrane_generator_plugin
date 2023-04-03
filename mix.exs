9980b15b452f099ff7eae604d124418c3b1f48cd
KarolK99 <konkolisz@gmail.com>
2023-04-03 17:39:06 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -37,9 +37,9 @@ defmodule Membrane.Generator.Plugin.Mixfile do
     [
       {:membrane_core, "~> 0.11.0"},
       {:membrane_raw_audio_format, "~> 0.10.0"},
-      {:membrane_raw_video_format, "~> 0.2"},
+      {:membrane_raw_video_format, "~> 0.3"},
       {:membrane_audio_mix_plugin, "~> 0.12.0", only: :test},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.25.0", only: :test},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.26.2", only: :test},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
